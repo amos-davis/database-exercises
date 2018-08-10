56627b09fe5893a641f9a455eb74abc4cdcaabc2
Amos Davis <amos.davis367@gmail.com>
2018-08-10 14:32:12 -0500

    first to commit

---
+++ tables-lesson.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE IF NOT EXISTS tables_lesson_db;
+
+
+USE table_lesson_db;
+
+
+DROP TABLE IF EXISTS flights;
+
+CREATE TABLE flights(
+  price Float DEFAULT 0.0,
+
+);
+
+
+SHOW TABLES;
