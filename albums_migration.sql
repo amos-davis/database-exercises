16198517fc09ba57c6f4ec56e2912d7443b9a1ef
Amos Davis <amos.davis367@gmail.com>
2018-08-10 15:34:33 -0500

    mysql tables exercise complete

---
+++ albums_migration.sql
@@ -0,0 +1,17 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums(
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  artist VARCHAR (255) NOT NULL,
+  name VARCHAR (255) NOT NULL,
+  release_date DATE NOT NULL,
+  sales FLOAT UNSIGNED,
+  genre VARCHAR (255),
+  PRIMARY KEY(id)
+);
+
+SHOW TABLES;
+DESCRIBE albums;
+SHOW CREATE TABLE albums;
--- tables-lesson.sql
+++ tables-lesson.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE IF NOT EXISTS tables_lesson_db;
+CREATE DATABASE IF NOT EXISTS table_lesson_db;
 
 
 USE table_lesson_db;
@@ -7,7 +7,7 @@ USE table_lesson_db;
 DROP TABLE IF EXISTS flights;
 
 CREATE TABLE flights(
-  price Float DEFAULT 0.0,
+  price Float DEFAULT 0.0
 
 );
 
